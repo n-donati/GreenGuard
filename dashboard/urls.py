37ee6ad296e26d528e690bbd711b1be1989c667c
Pedro Nieto <picopedro8@gmail.com>
2024-09-14 16:23:55 -0600

    add test example again...

---
+++ dashboard/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from pages import views as page_views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+
+] 
